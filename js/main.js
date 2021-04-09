34fb5a45b714c846801563bb376f247e5f11de4e
rohitgj <rohit@email.com>
2021-04-09 16:43:05 -0400

    done

--- js/main.js
+++ js/main.js
@@ -1,19 +1,36 @@
 const btn = document.querySelector("button");
 const ul = document.querySelector("ul");
 const input = document.querySelector("input");
+const list = document.querySelector(".content-2");
 
-document.getElementById('skill-list').innerHTML = '';
+
+
+document.getElementById("skill-list").innerHTML = "";
 
 btn.addEventListener("click", (e) => {
+  if (input.value === "") return;
   const li = document.createElement("li");
   console.log(input.value);
   li.textContent = input.value;
   ul.appendChild(li);
   input.value = "";
+  dltButton(li);
 });
 
-ul.addEventListener("click", (e) => {
-  const link = e.target;
-  console.log(link);
-  link.parentNode.removeChild(link);
+const dltButton = (li) => {
+  const dlt = document.createElement("button");
+  dlt.className = "xbutton";
+  dlt.innerHTML = "x";
+  li.prepend(dlt);
+};
+
+list.addEventListener("click", (e) => {
+  e.preventDefault();
+  const xButton = e.target;
+  console.log(xButton);
+  if (xButton.tagName === "BUTTON") {
+    const li = xButton.parentNode;
+    const ul = li.parentNode;
+    ul.removeChild(li);
+  }
 });
